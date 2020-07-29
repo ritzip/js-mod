0cd4cc88034b37ecaa3c1804391cb255af125b98
ritzip <49394064+ritzip@users.noreply.github.com>
2020-07-29 17:20:56 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+if(!this.global.done){
+    this.global.done = true;
+    const me = () => Vars.player;
+    const summon = (u, a, x, y, team) => {for(i = 0; i<a; i++){ un = u.create(team); un.set(x, y); un.add()}}
+
+    Events.on(EventType.PlayerChatEvent, cons(e=>{
+        if(e.message.split(" ")[0]=="!js"&&e.player.isAdmin){
+            var commands = [
+                "me = (() => {Vars.playerGroup.all().get(" + e.player.id + ")})",
+                "summon = ((u, a, x, y, team) => {for(i = 0; i<a; i++){ un = u.create(team); un.set(x, y); un.add()}})",
+                "findp = ((name) => {Vars.playerGroup.find(boolf(player => Strings.stripColors(player.name)==name))})",
+                e.message.slice(4, e.message.length)
+            ].join(";\n");
+
+            var msg;
+            try{
+                msg = Vars.mods.getScripts().runConsole(commands).slice(0, Vars.maxTextLength);
+            } catch(e){
+                msg = e.toString();
+            }
+
+            Core.app.post(run(() => Call.sendChatMessage(msg)));
+        }
+    }));
+}
